91e2fc309c36bf1eacad148ff54b904106dac7cb
Leonid <102907077+itsUltraWolf@users.noreply.github.com>
2023-10-27 18:55:43 +0300

    Пофикшен путь изображений

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
   {
     name: "Москва",
-    link: "/images/astemir-almov--kgrPSetNW8-unsplash.webp",
+    link: "./images/astemir-almov--kgrPSetNW8-unsplash.webp",
   },
   {
     name: "Санкт-Петербург",
-    link: "/images/daniil-smetanin-pjQyR8lYZw4-unsplash.webp",
+    link: "./images/daniil-smetanin-pjQyR8lYZw4-unsplash.webp",
   },
   {
     name: "Самара",
-    link: "/images/alexandra-nosova-uWVi5YHbtco-unsplash.webp",
+    link: "./images/alexandra-nosova-uWVi5YHbtco-unsplash.webp",
   },
   {
     name: "Казань",
-    link: "/images/alexey-ruban-JnlpvSKALl4-unsplash.webp",
+    link: "./images/alexey-ruban-JnlpvSKALl4-unsplash.webp",
   },
   {
     name: "Краснодар",
-    link: "/images/kirill-sirazheev-x14FSxm1YVA-unsplash.webp",
+    link: "./images/kirill-sirazheev-x14FSxm1YVA-unsplash.webp",
   },
   {
     name: "Сочи",
-    link: "/images/dima-fedorov-ZZkw00YCY1E-unsplash.webp",
+    link: "./images/dima-fedorov-ZZkw00YCY1E-unsplash.webp",
   },
 ];
