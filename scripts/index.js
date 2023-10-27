60cedf60828c57abc2835a684061f211798d88d7
Leonid <102907077+itsUltraWolf@users.noreply.github.com>
2023-10-27 18:46:22 +0300

    Написан JS-код

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
-// @todo: Темплейт карточки
+/* @todo: Темплейт карточки */
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: Функция создания карточки
+/* @todo: DOM узлы */
 
-// @todo: Функция удаления карточки
+//const profileAddButton = document.querySelector(".profile__add-button");
+const placesList = document.querySelector(".places__list");
 
-// @todo: Вывести карточки на страницу
+/* @todo: Функция создания карточки */
+
+function CardGenerator(cardImageSrc, cardTitleName) {
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = card.querySelector(".card__image");
+  const cardDeleteButton = card.querySelector(".card__delete-button");
+  const cardTitle = card.querySelector(".card__title");
+  //const cardLikeButton = card.querySelector(".card__like-button");
+
+  cardImage.src = cardImageSrc;
+  cardImage.alt = cardTitleName;
+  cardTitle.textContent = cardTitleName;
+
+  /* @todo: Функция удаления карточки */
+
+  cardDeleteButton.addEventListener("click", () => {
+    card.remove();
+  });
+
+  return card;
+}
+
+/* @todo: Вывести карточки на страницу */
+
+initialCards.forEach((cardItem) => {
+  const card = CardGenerator(cardItem.link, cardItem.name);
+
+  placesList.append(card);
+});
